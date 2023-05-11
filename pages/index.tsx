d02e796d54c1e8fc9828242dab33b126090a38a5
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2023-05-11 17:42:11 +0700

    T3FE05 feat:builđing UI for Restaurant

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400&display=swap" rel="stylesheet" />
       
       </Head>
     </>
---
+++ pages/restaurantowner/index.tsx
@@ -0,0 +1,75 @@
+
+import { Col, Rate, Row } from "antd";
+import style from "../../styles/restaurant/restaurant.module.scss";
+import Menu from "@/components/restaurant_owner/listMenu";
+import clsx from "clsx";
+import { useState } from "react";
+
+function RestaurentOwner() {
+
+    const [nav, setNav] = useState<any>()
+    return (
+        <>
+            <div className={style.page}>
+                <div className={style.banner}> </div>
+                <div className={style.head}>
+                    <div className={style.container}>
+
+
+                        <Row align={"bottom"} justify={"space-between"}>
+                            <Col >
+                                <div style={{ display: 'flex' }}>
+                                    <div className={style.avatar}>
+                                        <img width={'100%'} src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/066.jpg" alt="avatar" />
+                                    </div>
+                                    <div>
+                                        <div className={style.star}>
+                                            <Rate disabled defaultValue={6} />
+                                        </div>
+                                        <h2 className={style.name}>  Organic Arcadian Food</h2>
+                                        <p className={style.address}><span> <img width={22} src="https://marketplace.foodotawp.com/wp-content/themes/foodota/libs/images/map.png" alt="address" /></span> Main Boulevard, , Lahore, Punjab, Pakistan - 54000</p>
+                                    </div>
+                                </div>
+                            </Col>
+
+                            <Col >
+                                <div className={style.workingHour}>8:00AM - 9:00PM</div>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+                <div className={style.body}>
+                    <div className={style.container}>
+                        <Row gutter={[20, 10]}>
+                            <Col xxl={6} xl={6} lg={8} md={24}>
+                                <div className={style.nav}>
+                                    <h2>All Details</h2>
+                                    <div >
+                                        <span style={{ display: "inline-block", width: "40px", height: "2px", backgroundColor: "#FFD95A" }} ></span>
+                                        <span style={{ display: "inline-block", width: "2px", height: "2px", backgroundColor: "#FFD95A", marginLeft: '3px' }} ></span>
+                                        <span style={{ display: "inline-block", width: "2px", height: "2px", backgroundColor: "#FFD95A", marginLeft: '3px' }} ></span>
+                                        <span style={{ display: "inline-block", width: "2px", height: "2px", backgroundColor: "#FFD95A", marginLeft: '3px' }} ></span>
+                                    </div>
+                                    <div className={style.list}>
+                                        <div onClick={() => setNav(null)} className={clsx(style.item, !nav && style.active)}><img width={24} src="https://cdn-icons-png.flaticon.com/512/1046/1046747.png" alt="" /> Menu</div>
+                                        <div onClick={() => setNav('nav1')} className={clsx(style.item, nav === 'nav1' && style.active)}><img width={24} src="https://cdn-icons-png.flaticon.com/512/10674/10674751.png" alt="" /> Order</div>
+                                        <div onClick={() => setNav('nav2')} className={clsx(style.item, nav === 'nav2' && style.active)}><img width={24} src="https://cdn-icons-png.flaticon.com/512/5643/5643764.png" alt="" /> Process Order</div>
+                                        <div onClick={() => setNav('nav3')} className={clsx(style.item, nav === 'nav3' && style.active)}><img width={24} src="https://cdn-icons-png.flaticon.com/512/9849/9849587.png" alt="" /> Statictisc</div>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col flex="auto" >
+                                <div className={style.content}>
+                                    <Menu title='Menu' />
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+export default RestaurentOwner;
