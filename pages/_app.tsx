782b75baced98c26193d32550fb703b2ebf8f4cf
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2023-05-24 15:02:14 +0700

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const checkAuthorzite = () => {
+    
+  }
   return (
+
     <Layout>
       <Component {...pageProps} />
     </Layout>
