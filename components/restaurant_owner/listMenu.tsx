e5229b24929c6bb6f3bf533a64d0b0de35f1c1f4
damcongtri <105557938+damcongtri@users.noreply.github.com>
2023-05-28 01:39:12 +0700

    Fe/api crud food (#28)
    * fixbug
    
    * T3FE08 fest FE/api-crud-food

--- components/restaurant_owner/listMenu.tsx
+++ components/restaurant_owner/listMenu.tsx
@@ -1,20 +1,49 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import style from "./style/listMenu.module.scss"
 import { Button, Col, Divider, Popconfirm, Row, message } from "antd";
 import clsx from "clsx";
 import CreateProductModal from "./modelProduct";
+import RestaurentService from "@/service/restaurantService";
 const Menu: FC<any> = ({ title }) => {
     const [modalVisible, setModalVisible] = useState(false);
     const [productToEdit, setProductToEdit]: any = useState(null);
-    const handleCreate = (values: any, onSuccess: any) => {
+    const [listProduct, setListProduct] = useState([])
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+    //productService
+    const restaurentService = new RestaurentService
+
+    const success = (message: string) => {
+        messageApi.open({
+            type: 'success',
+            content: message,
+        });
+    };
+
+    const error = (message: string = 'Error! An error occurred. Please try again later') => {
+        messageApi.open({
+            type: 'error',
+            content: message,
+        });
+    };
+
+
+    const handleCreate = async (values: any, onSuccess: any) => {
         // Gửi request tạo sản phẩm đến API
         console.log("Form values", values);
-
-        setTimeout(() => {
-            // Giả sử request thành công và đóng modal
+        let [data, err] = values.id ? await restaurentService.editProduct(values, values.id) : await restaurentService.createProduct(values)
+        if (err) {
+            error('Error! An error occurred. Please try again later')
             onSuccess();
             setModalVisible(false);
-        }, 2000);
+        } else {
+            success(values.id ? 'Update successfuly' : 'Create successfuly')
+            await getAllProduct()
+            onSuccess();
+            setModalVisible(false);
+        }
+
     };
 
     const handleCancel = () => {
@@ -22,24 +51,40 @@ const Menu: FC<any> = ({ title }) => {
     };
 
     // Btn Delete
-    const confirm = (e: any): void => {
-        console.log(e);
-        message.success('Click on Yes');
+    const confirm = async (id: number) => {
+
+        let [data, err] = await restaurentService.detete(id)
+        getAllProduct()
+        err ? error() : message.success('Delete Done');
     };
 
     const cancel = (e: any): void => {
         console.log(e);
         message.error('Click on No');
     };
+    const getAllProduct = async () => {
+        let [data, err] = await restaurentService.getAllProduct()
+        if (!err) {
+            setListProduct(data)
+        }
+    }
 
+    useEffect(() => {
+        getAllProduct()
+    }, [])
+    console.log('listProduct', listProduct);
 
     return (
         <>
+            {contextHolder}
             <div className={style.menu}>
                 <Row justify={'space-between'} align={"middle"}>
                     <Col><h2>{title}</h2></Col>
                     <Col>
-                        <Button type="primary" style={{ backgroundColor: "#FFD95A", color: "#4C3D3D" }} onClick={() => setModalVisible(true)}>
+                        <Button type="primary" style={{ backgroundColor: "#FFD95A", color: "#4C3D3D" }} onClick={() => {
+                            setProductToEdit(null)
+                            setModalVisible(true)
+                        }}>
                             Create Product
                         </Button>
                     </Col>
@@ -52,48 +97,58 @@ const Menu: FC<any> = ({ title }) => {
                     <span style={{ display: "inline-block", width: "2px", height: "2px", backgroundColor: "#FFD95A", marginLeft: '3px' }} ></span>
                 </div>
                 <div className={style.list}>
-                    <div className={style.item}>
-                        <Row>
-                            <Col flex={'auto'}>
-                                <div style={{ display: 'flex' }}>
-                                    <div className={style.img}>
-                                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/04/oreo-shake-150x150.jpg" alt="coca" width={100} />
-                                    </div>
-                                    <div>
-                                        <div className={style.name}>Oreo Milk Shake</div>
-                                        <div className={style.price}>
-                                            <span className={style.sale}>$399.00</span>
-                                            $290.00
+                    {listProduct.map((item: any) =>
+                        <div className={style.item}>
+                            <Row>
+                                <Col flex={'auto'}>
+                                    <div style={{ display: 'flex' }}>
+                                        <div className={style.img}>
+                                            <img src={item.images} alt="coca" width={100} />
                                         </div>
+                                        <div>
+                                            <div className={style.name}>{item.name}</div>
+                                            {item.sale_price ? (
+                                                <div className={style.price}>
+                                                    <span className={style.sale}>${item.price}</span>
+                                                    ${item.sale_price}
+                                                </div>
+                                            ) : (
+                                                <div className={style.price} style={{ color: "yellowgreen" }}>
+                                                    ${item.price}
+
+                                                </div>
+                                            )}
+                                        </div>
+
                                     </div>
 
-                                </div>
+                                </Col>
+                                <Col>
+                                    <div className={style.btn}>
+                                        <Button type="primary" style={{ marginRight: "12px" }} className={clsx(true && style.activeOrder)} >{false ? 'Stop' : 'Start'} Order</Button>
+                                        <Button onClick={() => {
+                                            setModalVisible(true)
+                                            setProductToEdit(item)
+                                        }} type="primary" style={{ backgroundColor: "#FFD95A", color: "#4C3D3D", marginRight: "12px" }} >Edit</Button>
+                                        <Popconfirm
+                                            title="Delete the task"
+                                            description="Are you sure to delete this task?"
+                                            onConfirm={() => (confirm(item.id))}
+                                            onCancel={cancel}
+                                            okText="Yes"
+                                            cancelText="No"
+                                        >
+                                            <Button type="primary" danger>Delete</Button>
+                                        </Popconfirm>
 
-                            </Col>
-                            <Col>
-                                <div className={style.btn}>
-                                    <Button type="primary" style={{ marginRight: "12px" }} className={clsx(true && style.activeOrder)} >{false ? 'Stop' : 'Start'} Order</Button>
-                                    <Button onClick={() => {
-                                        setModalVisible(true)
-                                        setProductToEdit({ name: "aaa", price: 123, desc: "123" })
-                                    }} type="primary" style={{ backgroundColor: "#FFD95A", color: "#4C3D3D", marginRight: "12px" }} >Edit</Button>
-                                    <Popconfirm
-                                        title="Delete the task"
-                                        description="Are you sure to delete this task?"
-                                        onConfirm={confirm}
-                                        onCancel={cancel}
-                                        okText="Yes"
-                                        cancelText="No"
-                                    >
-                                        <Button type="primary" danger>Delete</Button>
-                                    </Popconfirm>
+                                    </div>
+                                </Col>
+                            </Row>
 
-                                </div>
-                            </Col>
-                        </Row>
+                            <Divider />
+                        </div>
+                    )}
 
-                        <Divider />
-                    </div>
 
                 </div>
             </div>
